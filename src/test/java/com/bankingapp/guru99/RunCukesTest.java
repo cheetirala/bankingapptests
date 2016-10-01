b2b3b8bc3bacd2d58012b48bbbc8df301084cad7
kotche <koteswara.cheetirala@unibet.com>
2016-10-01 15:23:35 +0100

    cucumber reports added

--- src/test/java/com/bankingapp/guru99/RunCukesTest.java
+++ src/test/java/com/bankingapp/guru99/RunCukesTest.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * Created by kotche on 22/09/2016.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/", format = {"pretty", "html:target/cucumber"})
+@CucumberOptions(features = "src/test/resources/features/", format = {"pretty", "json:target/cucumber.json"}, tags={"~@ignore"})
 public class RunCukesTest {
 }
